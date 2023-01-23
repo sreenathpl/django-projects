d0d3a24c17710a44dbe733ace6c91d536c446ea2
SREENATH P L <98871879+sreenathpl@users.noreply.github.com>
2023-01-23 11:11:07 +0530

    Delete ShoppyHub/projectShopping directory

--- ShoppyHub/projectShopping/appSearch/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-app_name = 'search_app'
-
-urlpatterns = [
-    path('search/',views.product_search,name='product_search'),
-]
--- ShoppyHub/projectShopping/appShopping/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-app_name = 'appShopping'
-
-urlpatterns = [
-    path('',views.allCategory, name='allCategory'),
-    path('<slug:c_slug>/',views.allCategory, name='allProductsCat'),
-    path('<slug:c_slug>/<slug:p_slug>',views.allProducts, name='detailsProduct'),
-    ]
